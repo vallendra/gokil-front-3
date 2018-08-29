5c1a97cb0ecfcb74735112e816874401dd0abd79
Kyky Farly Vallendra <kykyfarly@ymail.com>
2018-08-29 19:58:36 +0700

    Install Bootstrap-vue, Nuxt-validate, Axios. Configure Axios port and host.

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,9 +10,18 @@ module.exports = {
       { hid: 'description', name: 'description', content: 'Front-End Stack of Go-Kil Web App' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.2.0/css/all.css'}
     ]
   },
+  modules: [
+    '@nuxtjs/axios',
+    ['bootstrap-vue/nuxt', { css: false }],
+    ['nuxt-validate', {
+      lang: 'id'
+      // regular vee-validate options 
+    }]
+  ],
   /*
   ** Customize the progress bar color
   */
