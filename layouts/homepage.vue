010b053f26500a06d79fb66419c3285d3acecc5f
Kyky Farly Vallendra <kykyfarly@ymail.com>
2018-08-30 18:44:22 +0700

    Changing index, changing some layouts

---
+++ layouts/homepage.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+        <b-navbar toggleable="md" type="light" variant="info" class="bg-white sticky-top shadow">
+            <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+            <b-navbar-brand href="/"> <img src="~/assets/images/logo-black.png"  height="30" alt="GO-KIL" class="d-inline-block align-top"></b-navbar-brand>
+            <b-collapse is-nav id="nav_collapse">
+                <b-navbar-nav class="ml-auto">
+                <b-nav-item href="/">Beranda</b-nav-item>
+                <b-nav-item href="#">Tentang</b-nav-item>
+                <b-nav-item href="#">Kontak</b-nav-item>
+                <a class="btn btn-outline-primary" href="/login">MASUK</a>
+                </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
+        <nuxt />
+        <footer id="myFooter">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-3">
+                        <div class="container">
+                            <div class= "logo">
+                                <img src="@/assets/images/logo-white.png" alt="GO-KIL" height="40">
+                            </div>
+                        </div>
+                        <div class="container text-center">
+                            <small>Tak kenal maka tak sayang</small>
+                        </div>
+                        <div class="social-icons">
+                            <a href="#" class="twitter"><i class="fab fa-twitter fa-lg"></i></a>
+                            <a href="#" class="line"><i class="fab fa-line fa-lg"></i></a>
+                            <a href="#" class="google"><i class="fab fa-instagram fa-lg"></i></a>
+                        </div>                    
+                    </div>
+                    <div class="col-sm-3">
+                        <h5>Tentang</h5>
+                        <ul>
+                            <li><a href="#">Tim Kami</a></li>
+                            <li><a href="#">Hubungi Kami</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-3">
+                        <h5>Bantuan</h5>
+                        <ul>
+                            <li><a href="#">FAQ</a></li>
+                            <li><a href="#">Kebijakan Layanan</a></li>
+                            <li><a href="#">Kebijakan Privasi</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-3">
+                        <div class="btn-register">
+                            <a class="btn btn-primary btn-action" href="/signup" role="button" style="color: #333D3E;">DAFTAR SEKARANG</a>
+                        </div>
+                    </div>
+                    <div class="copyright col-sm-12">
+                        <p><small>Copyright © 2018 by GO-KIL Engineer.</small></p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,66 +1,62 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        go-kil-frontend
-      </h1>
-      <h2 class="subtitle">
-        Front-End Stack of Go-Kil Web App
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+  <div>
+    <!-- Carousel -->
+    <b-carousel id="carousel1"
+                controls
+                indicators
+                background="#ababab"
+                :interval="4000"
+                img-height="240"
+                img-width="1024">
+      <b-carousel-slide>
+        <div class="carousel-content">
+          <h1 class="thick">REVOLUSI OJEK<br>
+            BERADA DI TANGANMU
+          </h1>
+          <p>Mencari duit sudah sangat mudah sekarang.<br>
+          Hanya dengan menjadi mitra GO-KIL, kamupun bisa ikut kaya.</p>
+          <a role="button" class="btn btn-primary" href="#">BERGABUNG MENJADI MITRA</a>
+        </div>
+        <img slot="img" class="d-block img-fluid w-100 img-darken" width="1024" height="240"
+             src="../assets/images/moge-cantik.jpg" alt="mitra">
+      </b-carousel-slide>
+
+      <b-carousel-slide>
+            <div class="carousel-content">
+              <h1 class="thick">SAATNYA MERDEKA<br>
+              DARI MACET KOTAMU	
+              </h1>
+              <p>Kini telah hadir ojek online jaman <i>now</i>.<br>
+              Gak menuhin memori HP, cukup buka browser langsung cau!</p>
+              <a role="button" class="btn btn-primary" href="#">COBAIN GO-KIL SEKARANG!</a>
+            </div>
+            <img slot="img" class="d-block img-fluid w-100 img-darken" width="1024" height="240"
+             src="../assets/images/waiting-woman.jpg" alt="penumpang">
+      </b-carousel-slide>
+    </b-carousel>
+    <!-- What is Gokil? -->
+    <div class="bg-white align-items-center">
+      <div class="container text-center content">
+        <h1>Ojek Online Jaman <i>Now</i></h1>
+        <p>
+          rfgsrgf ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque cursus ex eget metus dignissim mollis. Nullam mollis ultricies elementum. Quisque imperdiet ex nisi, vel tempus erat volutpat non. Morbi egestas turpis sit amet accumsan sodales. Sed vel feugiat dolor, sit amet molestie lectus. Nunc mattis eros et libero malesuada, id auctor diam interdum. 
+        </p>  
       </div>
     </div>
-  </section>
+    <!-- Masih ragu? -->
+    <section class="bg-orange align-items-center">
+      <div class="container text-center content">
+        <h1 class="display-4">Masih rawgu?</h1>
+        <a class="btn btn-light btn-action btn-lg" href="/signup">DAFTAR AJA DULU
+        </a>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
 
 export default {
-  components: {
-    AppLogo
-  },
-  layout: 'navbar',
+  layout: homepage
 }
 </script>
-
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
-
