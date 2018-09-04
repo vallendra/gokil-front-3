8590369cf53bfc497b67e0c531e1e3ef68ed112e
Kyky Farly Vallendra <kykyfarly@ymail.com>
2018-09-04 22:31:21 +0700

    Change on OrderCustomer

--- components/OrderCustomer.vue
+++ components/OrderCustomer.vue
@@ -1,11 +1,17 @@
 <template>
     <b-container fluid class="bg-black shadow section">
         <div class="col-sm-12 col-md-11 col-lg-11 mx-auto section">
+            <!-- If costumer doesn't order yet -->
             <div v-if="this.$store.state.orderStep == 1">
                 <order-input />
             </div>
+            <!-- If costumer is searching a driver -->
             <div v-else-if="this.$store.state.orderStep == 2">
                 <order-search />
+            </div>
+            <!-- If costumer has an active order -->
+            <div v-else-if="this.$store.state.orderStep == 3">
+                <order-status />
             </div>
         </div>
     </b-container>
@@ -14,10 +20,12 @@
 <script>
 import OrderInput from '~/components/OrderInput'
 import OrderSearch from '~/components/OrderSearch'
+import OrderStatus from '~/components/OrderStatus'
 export default {
     components: {
         OrderInput,
-        OrderSearch
+        OrderSearch,
+        OrderStatus
     },
 }
 </script>
