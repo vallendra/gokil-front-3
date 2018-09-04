796d0b70e2fd88b6404b7c4a165d8e838e7415ea
Kyky Farly Vallendra <kykyfarly@ymail.com>
2018-09-04 15:11:57 +0700

    Minor adjustment at profile page

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,11 +1,32 @@
 <template>
     <div class="main-wrapper bg-black">
-        <div class="section">
-            <h1 class="text-white">Profil</h1>
-            <b-card class="bg-grey shadow">
+        <div class="text-center content">
+            <h1 class="text-white">Profile</h1>
+            <b-card class="col-sm-9 col-md-7 col-lg-5 mx-auto bg-grey shadow text-white">
                 <b-card-body>
                     <b-form>
-                        <b-form-input placeholder=""></b-form-input>
+                        <b-form-group>
+                            <label>Nama</label>
+                            <b-form-input placeholder="Masukan nama"></b-form-input>
+                        </b-form-group>
+                        
+                        <b-form-group>
+                            <label>Alamat</label>
+                            <b-form-input placeholder="Masukan alamat"></b-form-input>
+                        </b-form-group>
+                        
+                        <b-form-group>
+                            <label>Email</label>
+                            <b-form-input placeholder="Masukan email"></b-form-input>
+                        </b-form-group>
+
+                        <b-form-group>
+                            <label>Nomor telepon</label>
+                            <b-form-input placeholder="Masukan telepon"></b-form-input>
+                        </b-form-group>
+                        
+                        <button class="btn btn-lg btn-primary btn-block icon text-uppercase" type="submit">UBAH</button>
+
                     </b-form>
                 </b-card-body>
             </b-card>
@@ -14,8 +35,13 @@
 </template>
 
 <script>
+
 export default {
-    layout: 'user'
+    layout: 'user',
+//     async asyncData () {
+//     let { data } = await this.$axios.get('/users/current')
+//     return {name: data.name}
+//   }
 }
 </script>
 
