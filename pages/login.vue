26b233ed9a51503c1b60e5d0c9f5fe00cd7ef574
Kyky Farly Vallendra <kykyfarly@ymail.com>
2018-09-13 01:11:52 +0700

    Remove choose role button at login, change validation at dashboard

--- pages/login.vue
+++ pages/login.vue
@@ -12,18 +12,13 @@
             <div class="card shadow">
              <div class="card-body">
                <div class="text-center">Masuk sebagai <strong>{{$auth.$state.user.email}}</strong></div>
-               <button class="btn btn-lg btn-primary btn-block icon text-uppercase" type="submit" @click="logout">KELUAR</button>
+               <button class="btn btn-lg btn-primary btn-block icon text-uppercase" type="submit" @click="this.$router.push('/dashboard')">DASHBOARD</button>
+               <button class="btn btn-lg btn-secondary icon" @click="logout">KELUAR</button>
              </div>
             </div>
           </div>
           <div v-else>
-            
-            
             <div class="card shadow">
-              <div>
-                  <button id="button-customer" class="btn btn-lg width-50" v-bind:class="{active:customerActive, primary:customerActive}" @click="asCustomer">Masuk Sebagai Penumpang</button>
-                  <button id="button-driver" class="btn btn-lg width-50" v-bind:class="{active:driverActive, primary:driverActive}" @click="asDriver">Masuk Sebagai Pengemudi</button>
-              </div>
               <div class="card-body">
                 <b-alert show danger v-show="error !=null">
                   {{error}}
@@ -58,9 +53,7 @@ export default {
       userTypeID: 1,
       error: null,
       customerActive: true,
-      driverActive:false, 
-      customerType: 'primary',
-      driverType: 'secondary'
+      driverActive:false
     }
   },
   components: {
@@ -68,13 +61,6 @@ export default {
   },
   methods: {
     login: function () {
-      if (this.userTypeID=='') {
-        alert("Pilih dulu sebagai penumpang atau pengemudi ya");
-      }else if (this.userTypeID=='1') {
-        alert("Anda telah masuk sebagai penumpang");
-      }else if (this.userTypeID=='2') {
-        alert("Anda telah masuk sebagai Pengemudi");
-      }
       this.$auth.login({
         data: {
           user: {
@@ -86,17 +72,7 @@ export default {
     },
     logout: function () {
       this.$auth.logout().catch(e => {this.error = e + ''})
-    },
-    asDriver: function(){
-      this.userTypeID= 2;
-      this.customerActive = false;
-      this.driverActive = true;
-    },
-    asCustomer: function(){
-      this.userTypeID= 1;
-      this.customerActive = true;
-      this.driverActive = false;
-    },
+    }
   }
 }
 </script>
